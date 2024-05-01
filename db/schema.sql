9ae3cd36de0958ba0f2a8607300353b075bfec35
AlenaKhanis <alen4k245@gmail.com>
2024-05-01 20:23:33 +0300

    create file table

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS users (
     last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     type TEXT NOT NULL DEFAULT 'user'
 );
+
+DROP TABLE IF EXISTS files;
+CREATE TABLE IF NOT EXISTS files(
+    id INTEGER PRIMARY KEY NOT NULL,
+    filename TEXT NOT NULL,
+    filetype TEXT NOT NULL CHECK (filetype IN ('movie', 'song', 'episode', 'picture'))
+    fileSize INTEGER NOT NULL,
+    createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expirationDate TIMESTAMP,
+    descriotion TEXT,
+    owner INTEGER NOT NULL,
+    FOREIGN KEY (owner) REFERENCES users(id)
+);
+
