88843598c797b41e3d2162a644e68b02131d90e8
Ofer Sadan <ofersadan85@gmail.com>
2025-08-09 18:57:08 +0300

    remove all flask files

--- backend/tests/mock_tmdb.py
+++
@@ -1,5 +0,0 @@
-import json
-from pathlib import Path
-
-test_movies_data = json.loads(Path("tests/data/movies_by_revenue.json").read_text("utf-8"))
-test_one_movie_data = json.loads(Path("tests/data/infinity_war.json").read_text("utf-8"))
--- backend/tmdb.py
+++ backend/tmdb.py
@@ -1,18 +1,19 @@
+import logging
 import os
 from functools import cache, lru_cache
 
 import httpx
 from dotenv import load_dotenv
-from flask import current_app
 
 load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
+logger = logging.getLogger("uvicorn")
 
 
 @cache
 def get_movie_list():
-    current_app.logger.debug("Fetching movie list data from TMDB")
+    logger.debug("Fetching movie list data from TMDB")
     url = f"{TMDB_BASE_URL}/discover/movie?&sort_by=revenue.desc&api_key={TMDB_API_KEY}"
     response = httpx.get(url)
     movies_data = response.json()
@@ -21,7 +22,7 @@ def get_movie_list():
 
 @lru_cache(maxsize=128)
 def get_full_movie(id):
-    current_app.logger.debug(f"Fetching full movie data from TMDB ({id})")
+    logger.debug(f"Fetching full movie data from TMDB ({id})")
     url = f"{TMDB_BASE_URL}/movie/{id}?append_to_response=credits&language=en-US&api_key={TMDB_API_KEY}"
     response = httpx.get(url)
     movie_data = response.json()
